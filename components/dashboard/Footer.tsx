f88bd22beaf550e4bf0974bd12c77461062c9299
Safdar Chougle <chouglesafdar22@gmail.com>
2026-04-26 01:03:19 +0530

    added t&c and pp page

--- components/dashboard/Footer.tsx
+++ components/dashboard/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <footer className="flex bottom-0 relative w-full py-5 px-5 justify-center items-center text-center">
             <div className='flex items-center justify-center gap-0.5'>
                 <p className="text-[9px] md:text-[10px] xl:text-[11px] text-muted-foreground">&copy;{currentYear} All Rights Reserved GitPortify.</p>
-                <div className="flex items-center gap-2">
+                <div className="flex items-center gap-1.5">
                     <Link href="/privacy-policy" className="text-[9px] md:text-[10px] xl:text-[11px] text-muted-foreground hover:text-foreground transition">
                         Privacy Policy
                     </Link>
