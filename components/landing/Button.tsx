2d7b16133526639e4f06bd42f81588e311697c29
Safdar Chougle <chouglesafdar22@gmail.com>
2026-05-15 00:09:19 +0530

    minor bug updated

--- components/landing/Button.tsx
+++ components/landing/Button.tsx
@@ -24,7 +24,7 @@ export default function Button({
             whileHover={{ scale: 1.03 }}
             whileTap={{ scale: 0.97 }}
             transition={{ type: "spring", duration: 0.3 }}
-            className={`${reverse ? "flex-row-reverse" : ""} flex flex-row md:py-2.5 py-1.5 md:px-4 px-3 md:gap-2.5 gap-1.5 rounded-3xl lg:text-base md:text-sm sm:text-xs text-[12px] text-white bg-[#51149C] hover:bg-[#6D28D9] justify-center items-center cursor-pointer w-auto ${className}`}
+            className={`${reverse ? "flex-row-reverse" : ""} flex flex-row md:py-2.5 py-1.5 md:px-4 px-3 md:gap-2.5 gap-1.5 rounded-3xl lg:text-base md:text-sm sm:text-xs text-[12px] text-white bg-[#51149C] hover:bg-[#6D28D9] justify-center items-center cursor-pointer w-full ${className}`}
         >
             <span>{text}</span>
             {icon && <span>{icon}</span>}
