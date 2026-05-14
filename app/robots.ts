0dd122499503d64bf209a6f81a5aaf099a4d1b62
Safdar Chougle <chouglesafdar22@gmail.com>
2026-05-14 21:38:59 +0530

    added robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: {
+            userAgent: "*",
+            allow: "/",
+        },
+        sitemap: "https://gitportify.vercel.app/sitemap.xml",
+    };
+}
