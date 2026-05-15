dd7771c3319aaa753fdf3cfc89b69fe547327cae
Safdar Chougle <chouglesafdar22@gmail.com>
2026-05-15 16:57:21 +0530

    fixed minor buged

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -64,7 +64,7 @@ export default function Sidebar() {
                         <GoSidebarExpand />
                     </span>
                 </div>
-                <nav className="flex flex-col gap-2 space-y-2.5">
+                <nav className="flex flex-col gap-2">
                     {navLinks.map((link) => {
                         const active = pathname === link.href;
                         return (
