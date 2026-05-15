28c86d9e78d25428fc07c5008c5cadc6b870112b
Safdar Chougle <chouglesafdar22@gmail.com>
2026-05-15 14:38:35 +0530

    build navbar for moblie

--- components/landing/Navbar.tsx
+++ components/landing/Navbar.tsx
@@ -53,11 +53,10 @@ export default function Navbar() {
             {/* Mobile Menu */}
             <div
                 className={`absolute top-full left-0 w-full bg-background/30 backdrop-blur-md border-t border-border md:hidden overflow-hidden transition-all duration-300 ease-in-out
-    ${open
+                    ${open
                         ? "max-h-96 opacity-100 translate-y-0"
                         : "max-h-0 opacity-0 -translate-y-5 pointer-events-none"
-                    }
-    `}
+                    }`}
             >
                 <div className="flex flex-col items-center py-6 px-5 gap-5 fira-sans-regular lg:text-lg md:text-base sm:text-sm text-xs text-foreground">
 
