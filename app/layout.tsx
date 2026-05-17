82abb71de35177efdfe87724ae2ba6cfeea7a2a7
Safdar Chougle <chouglesafdar22@gmail.com>
2026-05-17 22:42:36 +0530

    added

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "GitPortify - Developer Portfolio Builder Platform Powered by GitHub",
   description: "Build and publish modern developer portfolios instantly using GitHub.",
+  verification:{
+google:"CmKds10cZMZYH-iisKQPsLzG9KgAGEq8aQxA0ru8q1I"
+  },
   keywords: [
     "developer portfolio",
     "portfolio builder",
@@ -48,7 +51,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning data-scroll-behavior="smooth">
       <head>
-        <meta name="google-site-verification" content="CmKds10cZMZYH-iisKQPsLzG9KgAGEq8aQxA0ru8q1I" />
         <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
       </head>
       <body
