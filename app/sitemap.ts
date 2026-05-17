1963e84e9fefd45c7a9afb38b89f40f87acbfebb
Safdar Chougle <chouglesafdar22@gmail.com>
2026-05-17 23:41:47 +0530

    added

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,21 +4,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
             url: "https://gitportify.vercel.app",
-            lastModified: new Date(),
-            changeFrequency:"weekly",
-            priority:1,
+            changeFrequency: "weekly",
+            priority: 1,
         },
         {
-            url: "https://gitportify.vercel.app/signup",
-            lastModified: new Date(),
+            url: "https://gitportify.vercel.app/signup"
         },
         {
-            url: "https://gitportify.vercel.app/privacy-policy",
-            lastModified: new Date(),
+            url: "https://gitportify.vercel.app/privacy-policy"
         },
         {
-            url: "https://gitportify.vercel.app/terms-and-conditions",
-            lastModified: new Date(),
+            url: "https://gitportify.vercel.app/terms-and-conditions"
         },
     ];
 };
